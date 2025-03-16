e8e8bd4e200cf1396827b7ee61146bb2dbcb362f
Andrés Muñoz <quemasandy@gmail.com>
2025-03-15 21:23:16 -0500

    Update nodejsDockerDSL.groovy

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -1,7 +1,7 @@
 job('Aplicacion Node.js Docker DSL') {
     description('Aplicación Node JS Docker DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
+        git('https://github.com/andy-mc/nodejsapp.git', 'master') { node ->
             node / gitConfigName('andy-mc')
             node / gitConfigEmail('quemasandy@gmail.com')
         }
